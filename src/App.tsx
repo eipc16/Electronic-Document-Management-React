4286ee6a3fa004ea525f1af0e03204c81a03a032
eipc16 <pprzemek.312@gmail.com>
2019-09-15 17:26:56 +0200

    created scalable sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,86 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Sidebar, { SidebarVisibility } from './sidebar/Sidebar';
+
+import { mdiAccessPoint, mdiAccount } from '@mdi/js';
+
 const App: React.FC = () => {
+
+  const elementsList = [
+  {
+    title: "Long title 1",
+    icon: mdiAccessPoint,
+    description: "1",
+    onclick: () => alert('1')
+  },
+  {
+    title: "Another long title",
+    icon: mdiAccount,
+    description: "3",
+    onclick: () => alert('2')
+  },  {
+    title: "Long title 3",
+    icon: mdiAccessPoint,
+    description: "4",
+    onclick: () => alert('1')
+  },  {
+    title: "Long title 4",
+    icon: mdiAccessPoint,
+    description: "5",
+    onclick: () => alert('1')
+  },  {
+    title: "Long title 5",
+    icon: mdiAccessPoint,
+    description: "6",
+    onclick: () => alert('1')
+  },  {
+    title: "Long title 6",
+    icon: mdiAccessPoint,
+    description: "7",
+    onclick: () => alert('1')
+  },  {
+    title: "Long title 7",
+    icon: mdiAccessPoint,
+    description: "8",
+    onclick: () => alert('1')
+  },  {
+    title: "Long title 8",
+    icon: mdiAccessPoint,
+    description: "9",
+    onclick: () => alert('1')
+  },  {
+    title: "Long title 9",
+    icon: mdiAccessPoint,
+    description: "11",
+    onclick: () => alert('1')
+  },  {
+    title: "Long title 10",
+    icon: mdiAccessPoint,
+    description: "12",
+    onclick: () => alert('1')
+  },  {
+    title: "Long title 11",
+    icon: mdiAccessPoint,
+    description: "13",
+    onclick: () => alert('1')
+  },  {
+    title: "Long title 12",
+    icon: mdiAccessPoint,
+    description: "14",
+    onclick: () => alert('1')
+  },  {
+    title: "Long title 13",
+    icon: mdiAccessPoint,
+    description: "15",
+    onclick: () => alert('14')
+  },
+  ]
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Sidebar elements={elementsList}/>
     </div>
   );
 }
---
+++ src/sidebar/Sidebar.tsx
@@ -0,0 +1,148 @@
+import React, { StatelessComponent, CSSProperties } from 'react'
+import ReactDOM, { render } from 'react-dom'
+
+import { mdiToggleSwitchOff, mdiToggleSwitch } from '@mdi/js'
+import { Icon } from '@mdi/react'
+
+import './SidebarStyles.scss'
+import { element } from 'prop-types';
+
+export enum SidebarVisibility {
+    HIDDEN = "hidden",
+    EXTENDED = "extended",
+    DEFAULT = "default"
+}
+
+export interface SidebarElement {
+    title: string,
+    icon: string,
+    description: string,
+    onclick: () => void
+}
+
+interface SidebarProps {
+    elements: SidebarElement[],
+    default_visiblity: SidebarVisibility,
+    scrollable?: boolean,
+    color?: string,
+    accent?: string,
+    loading_screen: () => JSX.Element
+}
+
+interface SidebarState {
+    visibility: SidebarVisibility;
+    selected: number;
+}
+
+class Sidebar extends React.Component<SidebarProps, SidebarState> {
+
+    public static DEFAULT_CLASS: string = "side-bar"
+
+    public static MODE_SWITCH_TITLE = "Switch sidebar mode"
+
+    public static defaultProps = {
+        default_visiblity: SidebarVisibility.DEFAULT,
+        scrollable: true,
+        color: "#0885de",
+        accent: "#54aff0",
+        loading_screen: () => {
+            return (
+                <div>Loading...</div>
+            )
+        }
+    }
+
+    constructor(props: any) {
+        super(props)
+
+        this.state = {
+            visibility: this.props.default_visiblity,
+            selected: 0
+        }
+    }
+
+    setVisibility(visibility: SidebarVisibility) {
+        this.setState({
+            visibility: visibility
+        })
+    }
+
+    setSelected(selected: number) {
+        this.setState({
+            selected: selected
+        })
+    }
+
+    toggleVisibilityMode() {
+        const newMode: SidebarVisibility = this.state.visibility == SidebarVisibility.DEFAULT
+                                        ? SidebarVisibility.EXTENDED
+                                        : SidebarVisibility.DEFAULT;
+
+        this.setVisibility(newMode);
+    }
+
+    private handleClick(event: React.MouseEvent, element: SidebarElement, index: number) {
+        event.preventDefault();
+        this.setSelected(index);
+        element.onclick();
+    }
+
+    private getElementsFromProps(): JSX.Element[] {
+
+        const iconClass = `action-icon ${this.state.visibility}`
+        const textClass = `action-title ${this.state.visibility}`
+
+        const elementArray = this.props.elements.map((el, index) => {
+
+            let elemStyle = {
+                backgroundColor: this.state.selected === index
+                                    ? this.props.accent
+                                    : this.props.color
+            }
+
+            return (
+                <div className='side-bar_element' style={elemStyle} key='element_${index}' onClick={(event) => this.handleClick(event, el, index)}>
+                    <Icon className={iconClass} path={el.icon} size={3} />
+                    <p className={textClass}>{el.title}</p>
+                </div>
+            )
+        })
+
+        return elementArray
+    }
+    
+    render() : JSX.Element {
+        const stateIcon: string = (this.state.visibility == SidebarVisibility.EXTENDED)
+                                        ? mdiToggleSwitchOff
+                                        : mdiToggleSwitch
+        
+        let visibility = this.state.visibility.valueOf();
+
+        const sidebarClass = `${Sidebar.DEFAULT_CLASS} ${visibility}`
+
+        const modeSwitchClass = `mode_switch ${visibility}`
+
+        const iconClass = `icon`
+
+        const textClass = `text ${visibility}`
+
+        const sidebarStyle = {
+            backgroundColor: this.props.color
+        }
+
+        return (
+            <div className={sidebarClass} style={sidebarStyle}>
+                <div className='elements_container'>
+                    {this.getElementsFromProps()}
+                </div>
+
+                <div className={modeSwitchClass} onClick={() => this.toggleVisibilityMode() }>
+                    <span className={textClass}>{Sidebar.MODE_SWITCH_TITLE}</span>
+                    <Icon className={iconClass} path={stateIcon} size={2} />
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Sidebar
